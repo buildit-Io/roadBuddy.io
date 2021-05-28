8266d6d87baae5cdf8a6cfb405bcf889598fe077
CaspianMike-L <looweesing@gmail.com>
2021-05-28 11:47:08 +0800

    rectifying static file error try 10 debugged and foubd out the collectstatc --noinput command defaults to staticfiles folder (for some reason), have tried to just use the default static files and tested collectstatic --noinput command locally without issues

--- roadBuddy/roadBuddy/settings/base.py
+++ roadBuddy/roadBuddy/settings/base.py
@@ -130,16 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-#print(PROJECT_ROOT)
-#print(STATIC_ROOT)
-#print(BASE_DIR)
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
