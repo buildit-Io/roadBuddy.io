5048259bbda7f4dad66d410df836c6965a65c4fb
CaspianMike-L <looweesing@gmail.com>
2021-05-28 11:36:40 +0800

    rectifying static file error try 8 - amended BASE_DIR

--- roadBuddy/roadBuddy/settings/base.py
+++ roadBuddy/roadBuddy/settings/base.py
@@ -16,7 +16,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
