29fce70c3426c292415abb16fc85a3028aef54e8
CaspianMike-L <looweesing@gmail.com>
2021-05-28 11:42:00 +0800

    rectifying static file error try 9 - manually assinging static root to staticfiles folder

--- roadBuddy/roadBuddy/settings/base.py
+++ roadBuddy/roadBuddy/settings/base.py
@@ -129,8 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+#print(PROJECT_ROOT)
+#print(STATIC_ROOT)
+#print(BASE_DIR)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
