62ba5a8044cc24390023ef9f3dda7e0f34abb496
CaspianMike-L <looweesing@gmail.com>
2021-05-28 11:01:25 +0800

    first heroku commit

--- roadBuddy/roadBuddy/settings/base.py
+++ roadBuddy/roadBuddy/settings/base.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+5g@3*44a4!3c8!rn8f%j*s0xv!ywj_v!pw(d6!$$1m5_b3^ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', None)
+if DEBUG is None:
+    DEBUG = False
+else:
+    if 'true' == DEBUG.lower():
+        DEBUG = True
+    else:
+        DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -124,3 +133,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
