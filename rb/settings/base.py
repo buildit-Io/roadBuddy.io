b89a55f016fc4ebcdd7b73d62f8c53d893e1daa6
CaspianMike-L <looweesing@gmail.com>
2021-05-30 17:05:48 +0800

    remove references to _secrets file in code

--- rb/settings/base.py
+++ rb/settings/base.py
@@ -40,7 +40,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY_1", secure.SECRET_KEY_1)
+SECRET_KEY = os.getenv("SECRET_KEY_1", SECRET_KEY_1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', None)
