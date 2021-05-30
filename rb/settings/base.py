288126a30ad0da68069c181413dc9ab9a5ba88e2
CaspianMike-L <looweesing@gmail.com>
2021-05-30 17:02:38 +0800

    made SECRET_KEY changes to base.py

--- rb/settings/base.py
+++ rb/settings/base.py
@@ -18,9 +18,14 @@ import dj_database_url
 # hid keys in local _secrets.py NOT PUSHED TO GITHUB
 try:
     import _secrets as secure
+    SECRET_KEY_1 = secure.SECRET_KEY_1
+    SECRET_KEY_3 = secure.SECRET_KEY_3
+    SECRET_KEY_4 = secure.SECRET_KEY_4
+    SECRET_KEY_5 = secure.SECRET_KEY_5
+    SECRET_KEY_6 = secure.SECRET_KEY_6
 except ImportError:
-    pass
-
+    SECRET_KEY_1,SECRET_KEY_3,SECRET_KEY_4,SECRET_KEY_5,SECRET_KEY_6 = "error_token"
+    
 # BASE_DIR and PROJECT_ROOT both refer to the location of the project root
 # - manually setted the path due to path issues caused by Path Object
 # - recttofoed issue by setting the path manually with the os library
